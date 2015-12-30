2705eace500269546140cfd1cfcbb31222f47937
david <lexuszhi1990@gmail.com>
2015-12-30 21:53:04 +0800

    use related_products label

--- app/overrides/add_product_relation_admin_sub_menu_tab.rb
+++ app/overrides/add_product_relation_admin_sub_menu_tab.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/_product_sub_menu',
   name: 'add_product_relation_admin_sub_menu_tab',
   insert_bottom: '[data-hook="admin_product_sub_tabs"]',
-  text: '<%= tab :relation_types, label: Spree::RelationType.to_s %>'
+  text: '<%= tab :relation_types, label: Spree.t(:related_products) %>'
 )
