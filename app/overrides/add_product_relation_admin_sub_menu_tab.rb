0ffad130ff2e415bb4fdbdd756d9263371b786f7
david <lexuszhi1990@gmail.com>
2016-01-04 19:17:36 +0800

    chang the name for tab

--- app/overrides/add_product_relation_admin_sub_menu_tab.rb
+++ app/overrides/add_product_relation_admin_sub_menu_tab.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/_product_sub_menu',
   name: 'add_product_relation_admin_sub_menu_tab',
   insert_bottom: '[data-hook="admin_product_sub_tabs"]',
-  text: '<%= tab :relation_types, label: Spree.t(:related_products) %>'
+  text: '<%= tab :relation_types, label: Spree.t(:relation_types, scope: [:admin, :tab]) %>'
 )
